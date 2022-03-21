c3cae99105d7c8a0d27e5337c5d952bee613fedb
DiogoPereiraMatos <a93234@alunos.uminho.pt>
2022-03-21 20:21:43 +0000

    Sugestão para ler as listas e funções. Parece que mudei muito mas só reorganizei as coisas.

--- src/lexer.py
+++ src/lexer.py
@@ -18,35 +18,50 @@ tokens = [
     "QUOTES", 
     "FIELD",
     "BLANK",
-    #"LISTSIZE",
+    "LISTSIZE",
+    "LISTFUNC"
     #"LISTLIMITATION"
 ]
 
 ################################################################################
 # TOKEN'S FUNCTIONS
 ################################################################################
+
+def t_QUOTES(t):
+    r'"'
+    t.lexer.begin("TEXTQUALIFIED")
+
+#TODO: Deve fazer interferência com o LISTSIZE e LISTFUNC (as in, deve fazer match com eles, não?) (A não ser que não deixemos ter {} e ::)
 def t_FIELD(t):
-    #Estou a dizer que tem de começar com um caractere e que dps pode ter espaços pelo meio
+    #Estou a dizer que tem de começar com um caracter e que dps pode ter espaços pelo meio
     r'[^\n",\ \t]([\ \t]*[^,\n\ \t]+)*'
-    t.lexer.fields.append(t.value)
-    
-def t_TEXTQUALIFIED_FIELD(t):
-    r'[^"]+'
     t.lexer.fields.append(t.value)
 
 def t_BLANK(t):
     r'(""|,[\ \t]*,|,\n)'
     t.lexer.fields.append("")
 
-def t_QUOTES(t):
-    r'"'
-    t.lexer.begin("TEXTQUALIFIED")
+def t_LISTSIZE(t):
+    r'{(\d+)(,\d+)?}'
+    field = t.lexer.fields[-1]
+    min_size = t.lexer.value(1)
+    max_size = t.lexer.value(2)
+    t.lexer.lists.append((field, min_size, max_size))
+
+def t_LISTFUNC(t):
+    r'::([a-zA-Z]+)'
+    field = t.lexer.fields[-1]
+    func = t.lexer.value(1)
+    t.lexer.funcs.append((field,func))
 
 
 def t_TEXTQUALIFIED_QUOTES(t):
     r'"'
     t.lexer.begin("INITIAL")
-
+    
+def t_TEXTQUALIFIED_FIELD(t):
+    r'[^"]+'
+    t.lexer.fields.append(t.value)
 
 
 def t_ANY_error(t):
@@ -64,6 +79,12 @@ def lexFunc(line, mode = "RECORD"):
 
     #New variable that will hold all the fields in a record
     lexer.fields = []
+
+    #Tuples (field, min_size, max_size)
+    lexer.lists = []
+
+    #Pairs (field, function)
+    lexer.funcs = []
 
     lexer.input(line)
 
