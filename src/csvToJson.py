ef8f6f1f3088decfec5d1e3b87bffe8654dec45e
Duarte Serrão <duarte.serrao.1999@gmail.com>
2022-03-26 18:04:40 +0000

    DONE

--- src/csvToJson.py
+++ src/csvToJson.py
@@ -18,26 +18,25 @@ def main():
     input = sys.argv[1]
     evalError(not input.endswith(".csv"), "Input file needs to end with '.csv'")
     
-    #output = sys.argv[2]
-    #evalError(not output.endswith(".json"), "Output file needs to end with '.json'")
+    output = sys.argv[2]
+    evalError(not output.endswith(".json"), "Output file needs to end with '.json'")
     
     fi = open(input, "r", encoding="UTF-8")
-    #fo = open(output, "w", encoding="UTF-8")
+    fo = open(output, "w", encoding="UTF-8")
 
     # Getting the header
     line = fi.readline()
     header = lexFunc(line, "HEADER")
-    print(header)
     
     # Reading the content in dictionary format
-    #content = readContent(fi, header)
+    content = readContent(fi, header)
     #content = readContent(fi, header, lists, funcs)
 
     # Writing in JSON file
-    #dicToJson(fo, content)
+    dicToJson(fo, content)
 
     fi.close()
-    #fo.close()
+    fo.close()
     return
 
 ################################################################################
@@ -52,130 +51,144 @@ def evalError(predicate, error = "Something ain't right"):
 ################################################################################
 # FUNCTION:  Getting each entry and put it in a list
 ################################################################################
-def readContent(file):
+def readContent(file, header):
     content = []
     for line in file:
         #lines with no content are skipped
         if line != "\n":
-            content.append(lexFunc(line))
+            record = contentToDic(lexFunc(line), header)
+            #If it returns an empty dictionary, then it skips the line
+            if record != {}:
+                content.append(record)
     return content
 
-def contentToDic(header, fields):
+
+
+
+################################################################################
+# FUNCTION:  
+################################################################################
+def contentToDic(fields, header):
     i = 0
+    dic = {}
     for category in header:
-        dic = {}
         # If it is just a string, then its a simple category
         if isinstance(category, str):
             dic[category] = fields[i]
         #If it isn't, then it involves a list
+            i += 1
         else:
             # If the size is a tuple, then we have a min and a max
-            if isinstance(category[2], tuple):
-                (size, max_size) = category[2]
+            if isinstance(category[1], tuple):
+                (min_size, size) = category[1]
             # if it is just a number, then thats the fixed size
             else:
-                size = category[2]
+                size = category[1]
                 
-            cat_list = fields[i:size]
-            if category[3] == None:
-                dic[category] = cat_list
+            cat_list = prepareList(fields[i:(i+size)])
+            
+            # updating size to the real value
+            size = len(cat_list)
+            
+            #if the record is missing values, then it is not valid
+            if size < min_size:
+                return {}
+            
+            # If there are no functions, then we just save the list
+            if len(category) < 3:
+                dic[category[0]] = cat_list
+                continue 
+            
+            if category[2] == "sum":
+                # SUM
+                result = 0
+                for value in cat_list:
+                    result += int(value)
+                dic[category[0] + "_" + category[2]] = result
+                
+            elif category[2] == "avg":
+                # AVERAGE
+                result = 0
+                for value in cat_list:
+                    result += int(value)
+                dic[category[0] + "_" + category[2]] = result / size
+                
+            # else is for invalid functions
             else:
-                dic[category[3]] = cat_list
-        i += 1
+                dic[category[0]] = cat_list
+                
+            i += size
+    return dic
 
+
+################################################################################
+# FUNCTION:  
+################################################################################
+def prepareList(input):
+    output = []
+    for elem in input:
+        if elem != '':
+            output.append(elem)
+    return output
 
 ################################################################################
 # FUNCTION:  Converting the list of entries to a json file
 ################################################################################
-def dicToJson(fo, content, header):
+def dicToJson(fo, content):
 
     # Begining of json file
     print("[", file=fo)
 
-    is_first = True
     # We iterate through every record except the last
-    for line in content:
-        i = 0
-        dic = {}
-        
-
-        if is_first:
-            print("\t{", file=fo)
-            is_first = False
-        else:
-            print(",\n\t{", file=fo)
-
-        for entry in header:
-            if entry in lists:
-                # É uma lista
-                min_size, max_size = lists[entry]
-
-                if entry in funcs:
-                    # Tem uma função aplicada
-                    func = funcs[entry]
-                    result = 0
-                    n = 0
-
-                    if func == "sum":
-                        # SUM
-                        for value in line[i:i+max_size]:
-                            if not value == "":
-                                result += int(value)
-                                n += 1
-
-                    elif func == "avg":
-                        # AVERAGE
-                        for value in line[i:i+max_size]:
-                            if not value == "":
-                                result = (result * n + int(value)) / (n+1)
-                                n += 1
-
-                    else:
-                        #função inválida (TODO)
-                        pass
-
-                    if n < min_size:
-                        #dados insuficientes (TODO)
-                        pass
-
-                    dic[entry] = result
-
-                else:
-                    # É uma lista normal
-                    result = removeEmpty(line[i:i+max_size])
-
-                    if len(result) < min_size:
-                        #dados insuficientes (TODO)
-                        pass
-
-                    dic[entry] = result
-
-                i += max_size
-            else:
-                # É uma entrada normal
-                dic[entry] = line[i]
-                i += 1
-
+    for dic in content[:-1]:
+        print("\t{", file=fo)
         printKeys(dic, fo)
-        print("\t}", file=fo, end="")
-        
+        print("\t},", file=fo)
+
+    # The last record will have a slightly different behaviour (no comma)
+    else:
+        print("\t{", file=fo)
+        printKeys(content[-1], fo)
+        print("\t}", file=fo)
+
     # End of json file
-    print("\n]", file=fo)
+    print("]", file=fo)
     return
+
 
 
 ################################################################################
 # FUNCTION:  Print to the file every entry of the dictionary
-# TODO: listas e resultados de funções não deverão ter ""
 ################################################################################
 def printKeys(dic, fo):
     keys = list(dic.keys())
+    
     # We iterate through every key except the last
     for entry in keys[:-1]:
-        print ("\t\t\"" + str(entry) + "\": \"" + str(dic[entry]) + "\",", file=fo)
-    # The last key will have a slightly different behaviour
+        value = dic[entry]
+        
+        # checking if this entry is a list
+        if isinstance(value, list):
+            print ("\t\t\"" + str(entry) + "\": [", file=fo, end ="")
+            #printing each element of the list
+            for elem in value[:-1]:
+                print(str(elem) + ", ", file=fo , end ="" )
+            else:
+                print(str(value[-1]) + "],", file=fo)
+        else:
+            print ("\t\t\"" + str(entry) + "\": \"" + str(value) + "\",", file=fo)
+    # The last key will have a slightly different behaviour (no comma)
     else:
-        print ("\t\t\"" + str(keys[-1]) + "\": \"" + str(dic[keys[-1]]) + "\"", file=fo)
+        entry = keys[-1]
+        value = dic[entry]
+        if isinstance(value, list):
+            print ("\t\t\"" + str(entry) + "\": [", file=fo, end ="")
+            for elem in value[:-1]:
+                print(str(elem) + ", ", file=fo, end ="")
+            else:
+                print(str(value[-1]) + "]", file=fo)
+        else:
+            print ("\t\t\"" + str(entry) + "\": \"" + str(value) + "\"", file=fo)
     return
 
 
--- src/lexer.py
+++ src/lexer.py
@@ -20,8 +20,8 @@ tokens = [
     "QUOTES", 
     "FIELD",
     "BLANK",
-    "LISTSIZE",
-    "LISTFUNC"
+    "LSIZE",
+    "FUNC"
 ]
 
 ################################################################################
@@ -29,83 +29,85 @@ tokens = [
 ################################################################################
 
 def t_QUOTES(t):
-    r',?"'
+    # the [\ \t]* is to ignore white characters
+    r',?[\ \t]*"'
     t.lexer.begin("TEXTQUALIFIED")
 
 def t_FIELD(t):
-    #Estou a dizer que tem de começar com um caracter e que dps pode ter espaços pelo meio
-    r'([^,\n\ \t{}]|::)([\ \t]*([^,\n\ \t{}]|::)+)*'
-    t.lexer.fields.append(t.value)
+    # First it needs a character, then it can have spaces in between and ends in a character
+    r',?[\ \t]*(?P<FIELD>[^,\n\ \t{}]([\ \t]*[^,\n\ \t{}]+)*)'
+    t.lexer.fields.append(str(t.lexer.lexmatch.group("FIELD")))
 
-#def t_BLANK(t):
-#    r'(""|,[\ \t]*,|,\n)'
-#    global list_counter
-#    print(str(list_counter) + "1")
-#    
-#    #r',?[\ \t]*(?P<content>\w+)[\ \t]*'    # ",   exemplo_1   ,"
-#    r',?(?P<content>\w+)'                   # ",exemplo_2,"
-#    #r',?(?P<content>[^,"{}:\n]+)'          # ",   exemplo   3   ,"
-#    t.lexer.fields.append(t.lexer.lexmatch.group("content"))
-
-#def t_BLANK(t):
-#    r',?""|,(?=[,\n])'
-#    t.lexer.fields.append("")
+def t_BLANK(t):
+    # Empty fields
+    r',?[\ \t]*""|,[\ \t]*(?=(,))|,[\ \t]*\n'
+    # This way it wont pick up the commas belongling to lists
+    if t.lexer.lexpos > t.lexer.pos:
+        if t.lexer.mode == "HEADER":
+            # If there is an empty field in the header, then we will create a category
+            t.lexer.fields.append("EMPTY_" + str(t.lexer.count_empties))
+            t.lexer.count_empties += 1
+        else:
+            t.lexer.fields.append("")
 
 # We need to have the @TOKEN because we need to use the variable
 # list_counter in the regular expression
 @lex.TOKEN(r'(,[\ \t]*){' + list_counter + r'}')
 def t_LIST_BLANK(t):
     global list_counter
-    print(str(list_counter) + "2")
+    # We need to know the end of the list
+    t.lexer.pos = t.lexer.lexpos + int(list_counter)
+    # resetting the list counter
     list_counter = str(1)
     t.lexer.begin('INITIAL')
 
 
-def t_LISTSIZE(t):
-    # the [\ \t]* is to ignore white characters
-    r'{[\ \t]*(?P<MIN>\d+)[\ \t]*(?:,[\ \t]*(?P<MAX>\d+)[\ \t]*)?}'
-    
-    # We need to begin the list state to read the consequent commas
-    t.lexer.begin('LIST')
-    
-    # Getting each group with values for the interval
-    match = t.lexer.lexmatch
-    min_size = match.group('MIN')
-    # We can't have the int function here, because max_size may be a NoneType
-    max_size = match.group('MAX')
-    
-    field = t.lexer.fields[-1]
-    # If the second group doesn't exist, then the min size is the only one
-    if max_size == None:
-        global list_counter
-        list_counter = str(min_size)
-        t.lexer.fields[-1] = (field, int(min_size))
-        
-        
-    else:
-        list_counter = str(max_size)
-        t.lexer.fields[-1] = (field, (int(min_size), int(max_size)))
+def t_LSIZE(t):
+    r'{[\ \t]*(?P<MIN>\d+)[\ \t]*(,[\ \t]*(?P<MAX>\d+)[\ \t]*)?}'
+    # Lists only exist in the header, not in records
+    if t.lexer.mode == 'HEADER':
+        # We need to begin the list state to read the consequent commas
+        t.lexer.begin('LIST')
+
+        # Getting each group with values for the interval
+        match = t.lexer.lexmatch
+        min_size = match.group('MIN')
+        # We can't have the int function here, because max_size may be a NoneType
+        max_size = match.group('MAX')
+
+        field = t.lexer.fields[-1]
+        # If the second group doesn't exist, then the min size is the only one
+        if max_size == None:
+            global list_counter
+            list_counter = str(min_size)
+            t.lexer.fields[-1] = (field, int(min_size))
+
+        else:
+            list_counter = str(max_size)
+            t.lexer.fields[-1] = (field, (int(min_size), int(max_size)))
         
 
-def t_LISTFUNC(t):
-    r'::(?P<op>[a-zA-Z]+)'
-    field = t.lexer.fields[-1]
-    func = t.lexer.lexmatch.group("op")
-    t.lexer.funcs[field] = func
+def t_LIST_FUNC(t):
+    r'::(?P<OP>[a-zA-Z]+)'
+    # We just need to add it to the end of the field list tuple by concatenation
+    t.lexer.fields[-1] += (t.lexer.lexmatch.group("OP"),)
 
 
 def t_TEXTQUALIFIED_QUOTES(t):
-    r'"'
+    r'"[\ \t]*'
     t.lexer.begin("INITIAL")
     
 def t_TEXTQUALIFIED_FIELD(t):
     r'([^"]|\\")+'
     t.lexer.fields.append(t.value)
 
-t_ANY_ignore = '\n,'
+t_LIST_INITIAL_ignore = ' \t\n' # The INITIAL state will also ignore spaces
+t_TEXTQUALIFIED_ignore = '\n'
+#t_ANY_ignore = '\n'
 
 def t_ANY_error(t):
     #it just skips over anything we don't care
+    #t.lexer.skip(1)
     print("Error: " + str(t))
 
 
@@ -114,8 +116,12 @@ def t_ANY_error(t):
 ################################################################################
 def lexFunc(line, mode = "RECORD"):
     lexer = lex.lex()
-    #if mode == "HEADER":
-    #   lexer.begin("HEADER")
+    
+    lexer.mode = mode
+    
+    lexer.count_empties = 0
+    
+    lexer.pos = 0
 
     # New variable that will hold all the fields in a record
     lexer.fields = []
