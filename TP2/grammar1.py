68711d3f16445fe40d45ae5d69601d17e231481f
DiogoPereiraMatos <a93234@alunos.uminho.pt>
2022-05-03 22:07:38 +0100

    fors com chavetas

--- TP2/grammar1.py
+++ TP2/grammar1.py
@@ -9,14 +9,17 @@ Sections    : Sections Section
 def p_sections_section(p):
     'Sections : Sections Section'
     p[0] = p[1] + [('section', p[2])]
+    #print('Sections : Sections Section')
 
 def p_sections_text(p):
     'Sections : Sections TEXT'
     p[0] = p[1] + [('text', p[2])]
+    #print('Sections : Sections TEXT')
 
 def p_sections_empty(p):
     'Sections : '
     p[0] = []
+    #print('Sections : ')
 
 
 """
@@ -25,6 +28,7 @@ Section : SECTIONBEGIN SectionContent SECTIONEND
 def p_section(p):
     'Section : SECTIONBEGIN SectionContent SECTIONEND'
     p[0] = p[2]
+    #print('Section : SECTIONBEGIN SectionContent SECTIONEND')
 
 
 """
@@ -33,32 +37,37 @@ SectionContent  : ListOps
 def p_sectioncontent(p):
     'SectionContent : ListOps'
     p[0] = p[1]
+    #print('SectionContent : ListOps')
 
 
 """
-ListOps : ListOps Op ";"
+ListOps : ListOps Op
         | 
 """
 def p_listops_op(p):
-    'ListOps : ListOps Op ";"'
+    'ListOps : ListOps Op'
     p[0] = p[1] + p[2]
+    #print('ListOps : ListOps Op')
 
 def p_listops_empty(p):
     'ListOps : '
     p[0] = []
+    #print('ListOps : ')
 
 
 """
 Op  : ForBlock
-    | "$" FormattedStr
+    | "$" FormattedStr ";"
 """
 def p_op_forblock(p):
     'Op : ForBlock'
     p[0] = p[1]
+    #print('Op : ForBlock')
 
 def p_op_formattedstr(p):
-    'Op : FORMATTEDSTRINGDELIMITER FormattedStr'
+    'Op : FORMATTEDSTRINGDELIMITER FormattedStr ";"'
     p[0] = p[2]
+    #print('Op : FORMATTEDSTRINGDELIMITER FormattedStr ";"')
 
 
 """
@@ -69,14 +78,17 @@ FormattedStr    : FormattedStr STR "$"
 def p_formattedstr_str(p):
     'FormattedStr : FormattedStr STR FORMATTEDSTRINGDELIMITER'
     p[0] = p[1] + [('str', p[2])]
+    #print('FormattedStr : FormattedStr STR FORMATTEDSTRINGDELIMITER')
 
 def p_formattedstr_id(p):
     'FormattedStr : FormattedStr ID FORMATTEDSTRINGDELIMITER'
     p[0] = p[1] + [('id', p[2])]
+    #print('FormattedStr : FormattedStr ID FORMATTEDSTRINGDELIMITER')
 
 def p_formattedstr_empty(p):
     'FormattedStr : '
     p[0] = []
+    #print('FormattedStr : ')
 
 
 """
@@ -86,10 +98,12 @@ ForBlock    : FOR "(" ID ":" ID ")" Op
 def p_forblock_iterate_single(p):
     'ForBlock : FOR "(" ID ":" ID ")" Op'
     p[0] = [('for', (p[3],p[5], p[7]))]
+    #print('ForBlock : FOR "(" ID ":" ID ")" Op')
     
 def p_forblock_iterate_mult(p):
     'ForBlock : FOR "(" ID ":" ID ")" "{" ListOps "}"'
     p[0] = [('for', (p[3],p[5], p[8]))]
+    #print('ForBlock : FOR "(" ID ":" ID ")" "{" ListOps "}"')
 
     
 def p_error(p):
--- TP2/lexer.py
+++ TP2/lexer.py
@@ -27,63 +27,63 @@ def t_NEWLINE(t):
 
 def t_section_FOR(t):
     r'for'
-    print(t)
+    #print(t)
     t.lexer.begin('foriterate')
     t.lexer.args = 2
     return t;
     
 def t_SECTIONBEGIN(t):
     r'/==='
-    print(t)
+    #print(t)
     t.lexer.begin('section')
     return t;
     
 def t_section_SECTIONEND(t):
     r'===/'
-    print(t)
+    #print(t)
     t.lexer.begin('INITIAL')
     return t;
     
 def t_section_COMMENTBEGIN(t):
     r'/\*'
-    print(t)
+    #print(t)
     t.lexer.push_state('commentblock')
     #return t;
     
 def t_commentblock_COMMENTEND(t):
     r'\*/'
-    print(t)
+    #print(t)
     t.lexer.pop_state()
     #return t;
 
 def t_section_FORMATTEDSTRINGDELIMITER(t):
     r'\$'
-    print(t)
+    #print(t)
     return t
     
     
     
 def t_TEXT(t):
     r'.+'
-    print(t)
+    #print(t)
     return t
 
 def t_section_STR(t):
     r'\"(?P<content>.*?)\"(?=\s*\$)'
     t.value = t.lexer.lexmatch.group('content')
-    print(t)
+    #print(t)
     return t
 
 def t_section_ID(t):
     r'[a-zA-Z_]\w*'
     #t.type = reserved.get(t.value, 'ID')
-    print(t)
+    #print(t)
     return t
 
 def t_foriterate_ID(t):
     r'[a-zA-Z_]\w*'
     #t.type = reserved.get(t.value, 'ID')
-    print(t)
+    #print(t)
     t.lexer.args = t.lexer.args - 1
     if t.lexer.args <= 0:
         t.lexer.begin('section')
@@ -91,12 +91,12 @@ def t_foriterate_ID(t):
     
 def t_section_COMMENTLINE(t):
     r'//[^\n]+'
-    print(t)
+    #print(t)
     #return t
     
 def t_commentblock_COMMENTBLOCK(t):
     r'(.|\n)+(?=\*/)'
-    print(t)
+    #print(t)
     #return t
     
 t_section_ignore = ' \n\t\r'
